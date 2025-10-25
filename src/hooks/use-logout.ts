ffd1ebfb56a548881a703b38a2eaed2885dd7a48
Andres Pizo <pizoluligoa@gmail.com>
2025-10-25 14:40:24 -0500

    fix: specify type for router in useLogout hook

--- src/hooks/use-logout.ts
+++ src/hooks/use-logout.ts
@@ -1,15 +1,15 @@
 import { useCallback } from "react";
 
+import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
 import { useRouter } from "next/navigation";
 
 export function useLogout() {
-  const router = useRouter();
+  const router: AppRouterInstance = useRouter();
 
   return useCallback(() => {
-    // Limpiar localStorage
     localStorage.removeItem("access_token");
+    localStorage.removeItem("user");
 
-    // Limpiar cookie
     document.cookie = "access_token=; path=/; max-age=0; SameSite=Lax";
 
     router.replace("/auth/v2/login");
